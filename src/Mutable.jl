2bc18a3045250b630dfe9bb15a1698659ced9c8f
Adrian Pop <adrian.pop@liu.se>
2020-01-29 20:53:09 +0100

    remove the specified types

--- src/Mutable.jl
+++ src/Mutable.jl
@@ -37,19 +37,19 @@
   """
 module Mutable
 
-mutable struct MutableType{T}
-  data::T
+mutable struct MutableType
+  data
 end
 
-function create(data::T)::MutableType{T} where {T}
+function create(data)::MutableType
   MutableType(data)
 end
 
-function update(mutable::MutableType{T}, data::T) where {T}
+function update(mutable::MutableType, data)
   mutable.data = data
 end
 
-function access(mutable::MutableType{T})::T where {T}
+function access(mutable::MutableType)
   mutable.data
 end
 
